1c249233e051aae1471a71973a39b261f1d12488
Ben Chik <benmaxium#gmail.com>
2026-04-28 20:33:16 +0800

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry == "yes":
     print("eat shit")
     print("eat pizza")
 else:
-    print('win')
+    thirsty = input("are you thirsty")
+    if thirsty == "yes":
+        print("drink water")
